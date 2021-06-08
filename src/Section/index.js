62bf0e56635146bfffdf4f9242dec36c0ac2f04d
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-08 15:09:10 +0200

    formated section

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,11 +3,12 @@ import "./style.css"
 
 const Section = ({ title, body, extraHeaderContent }) => (
     <section>
-        <header className="subHeader">
+        <header className="header">
             <h2>{title}</h2>
             {extraHeaderContent}
         </header>
         {body}
+
     </section>
 );
 
