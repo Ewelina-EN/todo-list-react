0e72d8bd906d3b4880c99e4031201d61b0940509
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-08 19:21:29 +0200

    rename section

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import "./style.css"
 
 const Section = ({ title, body, extraHeaderContent }) => (
-    <section>
-        <h2 className="subHeader"> {title} {extraHeaderContent}</h2>
-        {body}
+    <section className="section">
+        <header className="section__subHeader">
+            <h2 className="section__title"> {title} </h2>
+            {extraHeaderContent}
+        </header>
+        <div className="section__body">
+            {body}
+        </div>
+
     </section>
 );
 
