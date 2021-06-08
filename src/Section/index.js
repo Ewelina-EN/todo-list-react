58e1c8dcfaf8e111da662b74f9730342d8193cd3
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-08 14:31:41 +0200

    change style.css in section

--- src/Section/index.js
+++ src/Section/index.js
@@ -4,14 +4,10 @@ import "./style.css"
 const Section = ({ title, body, extraHeaderContent }) => (
     <section>
         <header className="subHeader">
-            <h2 className="subHeader__title">{title}</h2>
+            <h2>{title}</h2>
             {extraHeaderContent}
         </header>
-        <div>
-            {body}
-        </div>
-
-
+        {body}
     </section>
 );
 
