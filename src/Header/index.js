581e0edeba8284ba93fd8c90c03dac92a2575763
Ewelina Nieweglowska <nieweglowska.ewelina@gmail.com>
2021-06-08 13:37:37 +0200

    cleaning

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Header = ({title}) => (
+const Header = ({ title }) => (
     <h1>{title}</h1>
 );
 
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,21 +1,19 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({tasks, hideDoneTasks}) => (
+const Tasks = ({ tasks, hideDoneTasks }) => (
   <ul className="list">
     {tasks.map(task => (
       <li key={task.id}
-        className={`list__item ${
-          task.done & hideDoneTasks
+        className={`list__item ${task.done & hideDoneTasks
             ? " hide "
             : ""}`
         }>
         <button className="list__button list__button--done ">
           {task.done ? "✔" : ""}</button>
-        <span className={`list__task${
-          task.done 
-          ? " list__task--done" 
-          : ""}`}>
+        <span className={`list__task${task.done
+            ? " list__task--done"
+            : ""}`}>
           {task.content} </span>
         <button className="list__button list__button--remove ">🗑</button>
       </li>
