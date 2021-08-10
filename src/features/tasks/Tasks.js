b862e00bfed57b2b9e3ffafd93f6d3f2f7410029
Ewelina-EN <nieweglowska.ewelina@gmail.com>
2021-08-10 18:16:15 +0200

    code cleaning

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,44 +1,23 @@
 import React from 'react';
-import { useSelector } from "react-redux";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import StyledContainer from "../../common/Container";
-import { useTasks } from "../../useTasks";
-import { selectTasks } from './tasksSlice';
 
 function Tasks() {
-
-  const { tasks } = useSelector(selectTasks);
-
-  const {
-    // tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask,
-  } = useTasks();
-
   return (
     <StyledContainer>
       <Header title="Lista zadań" />
       <Section
         title="Dodaj nowe zadanie"
-        body={< Form addNewTask={addNewTask} />}
+        body={< Form />}
       />
       <Section
         title="Lista zadań"
-        body={
-          < TaskList
-            removeTask={removeTask}
-            toggleTaskDone={toggleTaskDone} />
-        }
-        extraHeaderContent={
-          <Buttons
-            setAllDone={setAllDone} />
-        }
+        body={< TaskList />}
+        extraHeaderContent={<Buttons />}
       />
     </StyledContainer>
   );
