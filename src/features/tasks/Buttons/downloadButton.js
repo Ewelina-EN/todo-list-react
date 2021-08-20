a2859d607daa23e6c2a700f3274b3bff928bc37f
Ewelina-EN <nieweglowska.ewelina@gmail.com>
2021-08-20 11:18:52 +0200

    moved download button to another section

---
+++ src/features/tasks/Buttons/downloadButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { StyledButtons, Button } from "./styled";
+import { fetchExampleTasks } from "../tasksSlice";
+
+const DownloadButton = () => {
+    const dispatch = useDispatch();
+    return (
+        <StyledButtons>
+            <>
+                <Button
+                    onClick={() => dispatch(fetchExampleTasks())}
+                >
+                    Pobierz przykładowe zadania
+                </Button>
+            </>
+        </StyledButtons>
+    );
+};
+export default DownloadButton;
